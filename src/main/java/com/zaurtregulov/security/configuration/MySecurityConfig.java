45afc066cfcf9d3a30ea2454fb20f938d8bf2d41
Belyankin Sergey <beljankin@yandex.ru>
2021-12-08 19:34:51 +0300

    курс трегулова

--- src/main/java/com/zaurtregulov/security/configuration/MySecurityConfig.java
+++ src/main/java/com/zaurtregulov/security/configuration/MySecurityConfig.java
@@ -22,12 +22,12 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
             auth.jdbcAuthentication().dataSource(dataSource);
-//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-//
-//        auth.inMemoryAuthentication()
-//                .withUser(userBuilder.username("zaur").password("zaur").roles("EMPLOYEE"))
-//                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
-//                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
+        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("zaur").password("zaur").roles("EMPLOYEE"))
+                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
+                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
 
     }
 
